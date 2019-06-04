962c39416a64b6c5d4c638e26cfac0ce8b305d38
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-04 16:12:11 -0700

    added process-update for wine price and temp and update wine price

---
+++ php/Wine/process-updateWinePrice.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../../connect.php';
+
+$wineID = $_POST['wineID'];
+$price = $_POST['price'];
+
+$conn = OpenCon();
+
+$sql = "update WineB set price = '$price' where wineID = '$wineID'";
+
+if ($conn->query($sql) === TRUE) {
+
+    echo "Record updated successfully";
+
+} else {
+
+    echo "Error updating record: " . $conn->error;
+
+}
+
+?>
---
+++ php/Wine/updateWinePrice.php
@@ -0,0 +1,50 @@
+<form action="process-updateWinePrice.php" method="post">
+
+    Update the price of a wineB
+
+    </br>
+
+    </br>
+
+    <label>Wine</label>
+
+    <?php
+
+    include '../../connect.php'; $conn = OpenCon();
+
+    $result = $conn->query("select wineID from WineB");
+
+    echo "<select name='wineID'>";
+
+    while ($row = $result->fetch_assoc())
+
+    {
+
+        unset($wineID);
+
+        $wineID = $row['wineID'];
+
+        echo '<option value="'.$wineID.'">'.'WineID: '.$wineID.'</option>';
+        // use '.' to append string
+
+    }
+
+    echo "</select>";
+
+    ?>
+
+    <br>
+
+
+
+    <label>Wine Price </label>
+
+    <input name="price" type="text" placeholder="Enter price for wine">
+
+    <br>
+
+    <br>
+
+    <input type="submit" value="Update">
+
+</form>
