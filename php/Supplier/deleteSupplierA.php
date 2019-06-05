dbc497b863f4b312d89b65bb3c96c41264c888e2
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-04 19:07:17 -0700

    did delete template

---
+++ php/Supplier/deleteSupplierA.php
@@ -0,0 +1,40 @@
+<form action="process-deleteSupplierA.php" method="post">
+
+    Delete a tuple with SupplierA tuple using name
+
+    </br>
+
+    </br>
+
+    <label>SupplierA</label>
+
+    <?php
+
+    include '../../connect.php'; $conn = OpenCon();
+
+    $result = $conn->query("select name, address from SupplierA");
+
+    echo "<select name='address'>";
+
+    while ($row = $result->fetch_assoc())
+
+    {
+
+        unset($name, $address);
+
+        $name = $row['name'];
+        $address = $row['address'];
+
+        echo '<option value="'.$address.'">'.'Name: '.$name.', Address: '.$address.'</option>';
+        // use '.' to append string
+
+    }
+
+    echo "</select>";
+
+    ?>
+
+
+    <input type="submit" value="Delete">
+
+</form>
---
+++ php/Supplier/process-deleteSupplierA.php
@@ -0,0 +1,21 @@
+<?php
+
+include '../../connect.php';
+
+$address = $_POST['address'];
+
+$conn = OpenCon();
+
+$sql = "delete from SupplierA where address = '$address'";
+
+if ($conn->query($sql) === TRUE) {
+
+    echo "Record updated successfully";
+
+} else {
+
+    echo "Error updating record: " . $conn->error;
+
+}
+
+?>
