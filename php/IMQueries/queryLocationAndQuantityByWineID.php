deb451e46967a9e6b846951842ef64aefd1f704e
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-05 15:01:07 -0700

    created some IMQueries (see README)

---
+++ php/IMQueries/queryLocationAndQuantityByWineID.php
@@ -0,0 +1,15 @@
+<form action="process-queryLocationAndQuantityByWineID.php" method="post">
+
+    Query LocationID and Quantity by WineID
+
+    </br>
+
+    </br>
+
+    <label>WineID</label>
+
+    <input type="text" name="wineID">
+
+    <input type="submit" value="Query">
+
+</form>
---
+++ php/IMQueries/queryPriceFromWineBByID.php
@@ -0,0 +1,15 @@
+<form action="process-queryPriceFromWineBByID.php" method="post">
+
+    Query WineB Price by WineID
+
+    </br>
+
+    </br>
+
+    <label>WineID</label>
+
+    <input type="text" name="wineID">
+
+    <input type="submit" value="Query">
+
+</form>
