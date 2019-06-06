ffdd2b5f2ffa17fa72dd3e09fcd8dc929a77cdbc
shirleywbi <swwbi@msn.com>
2019-06-06 12:12:58 -0700

    add default views (basic joins) for agRegion, restock, storageArea, storedIn, supplier, wine

---
+++ php/IMQueries/defaultView-agriculturalRegion.php
@@ -0,0 +1,26 @@
+<!-- Agricultural Region (with WineID)  -->
+
+<?php
+
+include '../../connect.php';
+include '../../template/input-query/create-table.php';
+
+$conn = OpenCon();
+
+$sql = "SELECT w.wineID, a.* 
+        FROM WineOrigin AS w 
+        INNER JOIN AgriculturalRegion AS a 
+        ON w.regionName = a.name";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+        myTable($conn, $sql);
+    } else {
+        echo "0 results";
+    }
+
+CloseCon($conn);
+
+?>
+
---
+++ php/IMQueries/defaultView-storedin.php
@@ -0,0 +1,25 @@
+<!-- StoredIn (with Wine Names) -->
+
+<?php
+
+include '../../connect.php';
+include '../../template/input-query/create-table.php';
+
+$conn = OpenCon();
+
+$sql = "SELECT s.wineID, w.brandName, s.locationID, s.quantityInLocation 
+        FROM WineB AS w 
+        INNER JOIN StoredIn AS s 
+        ON w.wineID = s.wineID;";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+        myTable($conn, $sql);
+    } else {
+        echo "0 results";
+    }
+
+CloseCon($conn);
+
+?>
