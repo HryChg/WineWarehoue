ecb7d0be91fd1c5ebf54fcdffb5b0935ff5ac0e8
harrychuang <vrjgik5@gmail.com>
2019-06-06 22:06:15 -0700

    can collect attributes dynamically and update order

--- php/OrderReceived/Process-Update.php
+++ php/OrderReceived/Process-Update.php
@@ -2,27 +2,70 @@
 
 include '../../connect.php';
 
-$orderID = $_POST['orderID'];
-$quantity = $_POST['quantity'];
+$conn = openCon();
 
-if (!$orderID || !$quantity){
-    echo 'variable not assigned';
-    return;
+// init all variable as empty str
+$orderID = mysqli_real_escape_string($conn, $_POST['orderID']);
+
+
+// get submitted keys in _POST
+$postedKeys = array_keys($_POST);
+
+// populate that attributes array with escaped strings
+$attributes = array();
+foreach ($postedKeys as $item) {
+    if ($item == 'orderID') continue; // skip adding orderID
+    $escapedStr = mysqli_real_escape_string($conn, $item);
+    array_push($attributes, $escapedStr);
+}
+
+// construct the sql string
+$partialStr = 'update OrderReceived ';
+for ($i = 0; $i < count($attributes) - 1; $i++) {
+    $newVal = $_POST[$attributes[$i]];
+    $partialStr = $partialStr."set $attributes[$i] = $newVal, ";
+}
+$lastIndex = count($attributes) - 1;
+$newVal = $_POST[$attributes[$lastIndex]];
+$partialStr = $partialStr."set $attributes[$lastIndex] = $newVal ";
+$partialStr = $partialStr." where orderID = $orderID";
+
+
+print_r($postedKeys);
+print_r($attributes);
+printf($partialStr);
+
+
+
+$sql = $partialStr;
+if ($conn->query($sql) === TRUE) {
+    echo '<br/>';
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+closeCon($conn);
+
+
+function collect($conn)
+{
+    if (mysqli_real_escape_string($conn, $_POST['backorder']) == 'Undefined') {
+        echo 'backorder is null';
+    }
 }
 
 
-$conn = OpenCon();
-
-$sql = "update OrderReceived set quantity = '$quantity' where orderID = '$orderID'";
-
-if ($conn->query($sql) === TRUE) {
-
-    echo "Record updated successfully";
-
-} else {
-
-    echo "Error updating record: " . $conn->error;
-
+function updateOrder($orderID, $col, $newVal)
+{
+    $conn = OpenCon();
+    $sql = "update OrderReceived set '$col' = '$newVal' where orderID = '$orderID'";
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+    closeCon($conn);
 }
 
 ?>
--- php/OrderReceived/View-Update.php
+++ php/OrderReceived/View-Update.php
@@ -1,3 +1,26 @@
+<?php
+function displayOrderAttributes(){
+    $conn = OpenCon();
+    $result = $conn->query("SHOW COLUMNS FROM OrderReceived");
+    if (!$result) {
+        echo 'Could not show columns from OrderReceived';
+        exit;
+    }
+    echo "<select name='column' class='ui dropdown'>";
+    while ($row = $result->fetch_assoc())
+    {
+        unset($type);
+        $type = $row['Field'];
+        if ($type == 'orderID') continue;
+        echo '<option value="'.$type.'">'.$type.'</option>';
+    }
+    echo "</select>";
+    CloseCon($conn);
+}
+
+?>
+
+
 <div class="container">
 
     <form action="../../php/OrderReceived/Process-Update.php" method="post">
@@ -34,6 +57,9 @@
         <label>Order Quantity </label>
 
         <input name="quantity" type="text" placeholder="Enter new quantity">
+
+        <br/>
+<!--        --><?php //displayOrderAttributes() ?>
 
         <div class="row center">
             <input class="waves-effect waves-light btn" type="submit" value="Update">
