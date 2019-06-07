52db738e8d2dc677df858f15635472022e19c2d8
harrychuang <vrjgik5@gmail.com>
2019-06-07 00:41:08 -0700

    basic styling of the add form

--- php/OrderReceived/View-Add.php
+++ php/OrderReceived/View-Add.php
@@ -1,61 +1,43 @@
-<div class="container">
-    <form action="../../php/OrderReceived/Process-Add.php" method="post">
-        <h4>Add Order</h4>
-        <div class="row">
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="orderID" type="text" class="validate">
-                    <label for="orderID">orderID</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="employeeID" type="text" class="validate">
-                    <label for="employeeID">employeeID</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="wineID" type="text" class="validate">
-                    <label for="wineID">wineID</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="quantity" type="text" class="validate">
-                    <label for="quantity">quantity</label>
-                </div>
-            </div>
+<h1 class="ui header">Add Order</h1>
+<form class="ui form" action="../../php/OrderReceived/Process-Add.php" method="post">
+    <div class="four fields">
+        <div class="field">
+            <input placeholder="" name="orderID" type="text" class="validate">
+            <label for="orderID">orderID</label>
         </div>
-        <div class="row">
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="retailer" type="text" class="validate">
-                    <label for="retailer">retailer</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="address" type="text" class="validate">
-                    <label for="address">address</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="backorder" type="text" class="validate">
-                    <label for="backorder">backorder</label>
-                </div>
-            </div>
-            <div class="col s3">
-                <div class="input">
-                    <input placeholder="" name="orderReceivedDate" type="text" class="validate">
-                    <label for="orderReceivedDate">orderReceivedDate</label>
-                </div>
-            </div>
+        <div class="field">
+            <input placeholder="" name="employeeID" type="text" class="validate">
+            <label for="employeeID">employeeID</label>
         </div>
-        <div class="row center">
-            <input class="waves-effect waves-light btn" type="submit" value="Add">
+        <div class="field">
+            <input placeholder="" name="wineID" type="text" class="validate">
+            <label for="wineID">wineID</label>
         </div>
+        <div class="field">
+            <input placeholder="" name="quantity" type="text" class="validate">
+            <label for="quantity">quantity</label>
+        </div>
+    </div>
+    <div class="four fields">
+        <div class="field">
+            <input placeholder="" name="retailer" type="text" class="validate">
+            <label for="retailer">retailer</label>
+        </div>
+        <div class="field">
+            <input placeholder="" name="address" type="text" class="validate">
+            <label for="address">address</label>
+        </div>
+        <div class="field">
+            <input placeholder="" name="backorder" type="text" class="validate">
+            <label for="backorder">backorder</label>
+        </div>
+        <div class="field">
+            <input placeholder="" name="orderReceivedDate" type="text" class="validate">
+            <label for="orderReceivedDate">orderReceivedDate</label>
+        </div>
+    </div>
 
-    </form>
-</div>
+    <input class="waves-effect waves-light btn" type="submit" value="Add">
+
+</form>
+
