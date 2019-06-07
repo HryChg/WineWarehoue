2c4ff6f50a35fb6c887805a51a59492cc4f4ab4d
harrychuang <vrjgik5@gmail.com>
2019-06-06 19:55:06 -0700

    extracted method out from HTML code

--- php/Employee/View-Update.php
+++ php/Employee/View-Update.php
@@ -1,3 +1,46 @@
+<?php
+function displayEmployeeOption(){
+    $conn = OpenCon();
+    $result = $conn->query("SELECT employeeID, name From Employee");
+    echo "<select name='employeeID' class='ui dropdown'>";
+    while ($row = $result->fetch_assoc())
+    {
+        unset($employeeID, $name);
+        $employeeID = $row['employeeID'];
+        $name = $row['name'];
+        echo '<option value="'.$employeeID.'">'.'EmployeeID: '.$employeeID.' '.$name.'</option>';
+    }
+    echo "</select>";
+    CloseCon($conn);
+}
+
+function updateEmployeeName(){
+    $employeeID = $_POST['employeeID'];
+    $name = $_POST['name'];
+
+
+    if (is_null($employeeID) and is_null($name)){
+        echo 'variable not assigned';
+    }
+
+    if (isset($employeeID) and isset($name)){
+        $conn = OpenCon();
+
+        $sql = "update Employee set name = '$name' where employeeID = '$employeeID'";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+
+        CloseCon($conn);
+        return;
+    }
+}
+?>
+
+
 <div class="container">
     <form action="../../php/Employee/Process-Update.php" method="post">
         <h4>Update Employee</h4>
@@ -6,30 +49,9 @@
         <br/>
         <label>Employee</label>
 
-        <?php
-
-        $conn = OpenCon();
-
-        $result = $conn->query("SELECT employeeID, name From Employee");
-
-        echo "<select name='employeeID' class='ui dropdown'>";
-
-        while ($row = $result->fetch_assoc())
-        {
-            unset($employeeID, $name);
-            $employeeID = $row['employeeID'];
-            $name = $row['name'];
-            echo '<option value="'.$employeeID.'">'.'EmployeeID: '.$employeeID.' '.$name.'</option>';
-        }
-
-        echo "</select>";
-
-        CloseCon($conn);
-
-        ?>
+        <?php displayEmployeeOption() ?>
 
         <br>
-
 
 
         <label>Employee New Name</label>
