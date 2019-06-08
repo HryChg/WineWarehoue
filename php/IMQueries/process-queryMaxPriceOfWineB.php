0dd4d6fa3d11c37c2debe5354e0dae3e40ff7f34
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-07 20:55:58 -0700

    mixed min max queries

--- php/IMQueries/process-queryMaxPriceOfWineB.php
+++ php/IMQueries/process-queryMaxPriceOfWineB.php
@@ -5,8 +5,11 @@ include '../../template/input-query/create-table.php';
 
     $conn = OpenCon();
 
-    $sql = "SELECT wineID, MAX(price)
-            FROM WineB";
+    $sql = "SELECT wineID, brandName, grapeType1, grapeType2, price 
+            FROM WineB AS p 
+            INNER JOIN 
+            ( SELECT MAX(price) AS max FROM WineB ) AS m 
+            ON p.price = m.max";
 
     $result = $conn->query($sql);
 
--- php/IMQueries/process-queryMinPriceByBrand.php
+++ php/IMQueries/process-queryMinPriceByBrand.php
@@ -7,9 +7,14 @@ $conn = OpenCon();
 
 $brandName = $_POST['brandName'];
 
-$sql = "SELECT brandName, MIN(price)
-	FROM wineB
-	WHERE brandName = '$brandName'";
+$sql = "select w.brandName, w.grapeType1, w.grapeType2, w.price
+from 
+(select brandName, grapeType1, grapeType2, price from wineb where brandName = '$brandName') w
+INNER JOIN
+(SELECT brandName, MIN(price) AS min from wineb GROUP BY brandName) m
+ON w.brandName = m.brandName AND w.price = m.min";
+
+
 
 $result = $conn->query($sql);
 
