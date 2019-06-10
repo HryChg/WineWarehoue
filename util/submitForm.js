f8642aad05291a004e36c826992b71ef93a2a7dc
shirleywbi <swwbi@msn.com>
2019-06-09 22:45:43 -0700

    update instructions on how to use submit form

--- util/submitForm.js
+++ util/submitForm.js
@@ -1,7 +1,18 @@
 // Within form, add relative url path to the process method
 // Add the following to your form to use:
-// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-// <script src="./submitForm.js"></script>
+//  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+//  <script src="./submitForm.js"></script>
+// Add this jquery script after your form:
+// - submit button must have class: submit-form
+// - the include php in your ui must be wrapped in a div (in this case, id: "storedin-table")
+// - load the view file for your table
+{/* <sscript>
+$(document).ready(function() {
+    $(".submit-form").click(function(e) {
+        $("#storedin-table").load(\'../../php/StoredIn/defaultView-storedin.php\');
+    });
+});
+</script>' */}
 
 $(document).ready(function() {
     $(".submit-form").click(function(e) {
