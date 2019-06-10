6b80d53fa875333c4aebbc08cbe185c166c99802
shirleywbi <swwbi@msn.com>
2019-06-10 15:56:05 -0700

    fix agricultural region -- add back selection

--- php/AgriculturalRegion/updateRegion.php
+++ php/AgriculturalRegion/updateRegion.php
@@ -2,6 +2,23 @@
 
     <h3>Update Agricultural Region</h3>
 
+    <label>Agricultural Region</label>
+
+    <?php
+
+    include_once '../../connect.php'; 
+    $conn = OpenCon();
+    $result = $conn->query("select name from AgriculturalRegion");
+
+    echo "<p><select name='name'>";
+    echo '<option value="">---Select name---</option>';
+    while ($row = $result->fetch_assoc()) {
+        unset($name);
+        $name = $row['name'];
+        echo '<option value="'.$name.'">'.$name.'</option>';
+    }
+    echo "</select></p>";
+    ?>
     <p>
         <label>Temperature </label>
         <input name="temperature" type="text" placeholder="Enter new temperature">
