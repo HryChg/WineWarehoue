34f223482f19fef1bdc7ef24cf4136a4de705093
harrychuang <vrjgik5@gmail.com>
2019-06-14 16:11:04 -0700

    styled returned shipment

--- php/ReturnedShipment/add.php
+++ php/ReturnedShipment/add.php
@@ -1,82 +1,59 @@
-	<?php 
+<?php
 
-	//Add
-	$shipmentID = $returnID = $returnedQuantity ='';
-	$errors = '';
-
-
-	if(isset($_POST['submit']))
-	{
-		if(empty($_POST['shipmentID']) or 
-			empty($_POST['returnID']) or 
-			empty($_POST['returnedQuantity']))
-		{
-			$errors = 'All fields are required';
-		}
-
-		if($errors)
-		{
-			//error is echoed
-		} else {
-			$shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
-			$returnID = mysqli_real_escape_string($conn, $_POST['returnID']);
-			$returnedQuantity = mysqli_real_escape_string($conn, $_POST['returnedQuantity']);
-			// create sql
-			$sql = "INSERT INTO ReturnedShipment VALUES('$shipmentID', '$returnID', '$returnedQuantity')";
-			// save to db and check
-			if(mysqli_query($conn, $sql))
-			{
-				// success
-				$success = 'Added!';
-				echo "<meta http-equiv='refresh' content='0'>";
-
-			} else {
-				echo 'query error: '. mysqli_error($conn);
-			}
-		}
-	}
+//Add
+$shipmentID = $returnID = $returnedQuantity = '';
+$errors = '';
 
 
-	?>
+if (isset($_POST['submit'])) {
+    if (empty($_POST['shipmentID']) or
+        empty($_POST['returnID']) or
+        empty($_POST['returnedQuantity'])) {
+        $errors = 'All fields are required';
+    }
 
-	<!DOCTYPE html>
-	<html>
+    if ($errors) {
+        //error is echoed
+    } else {
+        $shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
+        $returnID = mysqli_real_escape_string($conn, $_POST['returnID']);
+        $returnedQuantity = mysqli_real_escape_string($conn, $_POST['returnedQuantity']);
+        // create sql
+        $sql = "INSERT INTO ReturnedShipment VALUES('$shipmentID', '$returnID', '$returnedQuantity')";
+        // save to db and check
+        if (mysqli_query($conn, $sql)) {
+            // success
+            $success = 'Added!';
+            echo "<meta http-equiv='refresh' content='0'>";
 
-	<section class="container grey-text">
-		<h1 class="ui header">Add a Return Shipment</h1>
-		<form class="white" action="../../ui/ShippingManager/index.php" method="POST">
-			<div class="row">
-				<div class="col s3">
-					<div class="input">
-						<input type="text" name="shipmentID" value="<?php echo htmlspecialchars($shipmentID) ?>">
-						<label>shipmentID</label>
-					</div>
-				</div>
-
-
-				<div class="col s3">
-					<div class="input">
-						<input type="text" name="returnID" value="<?php echo htmlspecialchars($returnID) ?>">
-						<label>returnID</label>
-					</div>
-				</div>
-
-
-				<div class="col s3">
-					<div class="input">
-						<input type="text" name="returnedQuantity" value="<?php echo htmlspecialchars($returnedQuantity) ?>">
-						<label>Returned Quantity</label>
-					</div>
-				</div>
+        } else {
+            echo 'query error: ' . mysqli_error($conn);
+        }
+    }
+}
 
 
-			</div>
-			<div class="red-text"><?php echo $errors; ?></div>
-			<div class="center">
-				<input type="submit" name="submit" value="Add" class="waves-effect waves-light btn">
-			</div>
-		</form>
-	</section>
+?>
 
 
-	</html>
+<h1 class="ui header">Add a Return Shipment</h1>
+<div class="ui segment">
+<form class="ui form" action="../../ui/ShippingManager/index.php" method="POST">
+
+    <div class="field">
+        <input type="text" name="shipmentID" value="<?php echo htmlspecialchars($shipmentID) ?>">
+        <label>shipmentID</label>
+    </div>
+    <div class="field">
+        <input type="text" name="returnID" value="<?php echo htmlspecialchars($returnID) ?>">
+        <label>returnID</label>
+    </div>
+    <div class="field">
+        <input type="text" name="returnedQuantity" value="<?php echo htmlspecialchars($returnedQuantity) ?>">
+        <label>Returned Quantity</label>
+    </div>
+    <div class="red-text"><?php echo $errors; ?></div>
+    <input type="submit" name="submit" value="Add" class="ui primary button">
+
+</form>
+</div>
--- php/ReturnedShipment/index.php
+++ php/ReturnedShipment/index.php
@@ -1,9 +1,42 @@
 <?php
+//
+//include 'view_table.php';
+//
+//include 'add.php';
+//
+//include 'update.php';
+//
+//?>
 
-include 'view_table.php';
 
-include 'add.php';
-
-include 'update.php';
-
-?>
+<div class="ui grid centered">
+    <div class="fourteen wide column">
+        <?php include 'view_table.php'; ?>
+    </div>
+    <div class="row">
+        <div class="five wide column">
+            <?php include 'add.php'; ?>
+        </div>
+        <div class="five wide column">
+            <?php include 'update.php'; ?>
+        </div>
+        <div class="five wide column">
+            <h1>Delete Return Shipment <br>(Work In Progress)</h1>
+            <div class="ui segment">
+                <div class="ui placeholder">
+                    <div class="image header">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="paragraph">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
