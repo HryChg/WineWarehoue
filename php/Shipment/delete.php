28d3e08736c6a006f2ca71be3d2f1616d9240066
harrychuang <vrjgik5@gmail.com>
2019-06-14 13:06:01 -0700

    delete Shipment Implemented

---
+++ php/Shipment/delete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<section>
+    <h1 class="ui header">Delete Shipment</h1>
+    <div class="ui segment">
+        <form class="ui form container" action="../../php/Shipment/process-delete.php" method="POST">
+            <div class="fields">
+                <div class="field">
+                    <input name="shipmentID" type="text">
+                    <label for="shipmentID">shipmentID</label>
+                </div>
+            </div>
+            <input class="ui red button" type="submit" value="Delete">
+        </form>
+    </div>
+</section>
