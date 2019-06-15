d1346f1ac8fdfabd0015c5b027d8b0407db48756
shirleywbi <swwbi@msn.com>
2019-06-15 03:29:33 -0700

    merge delete wines - works for brandname and wineID, issues with wineOrigin because of foreign key

--- php/Wine/deleteWine.php
+++ php/Wine/deleteWine.php
@@ -1,7 +1,7 @@
 <script src="./../../php/Wine/delete-wine.js"></script>
 <form class="ui form" id="delete-wine" url="../../php/Wine/process-deleteWine.php" method="post">
 
-    <h3>Delete Wine</h3>
+    <h3>Delete Wines</h3>
 
     <label>BrandName</label>
 
@@ -10,6 +10,7 @@
     include_once '../../connect.php'; 
     $conn = OpenCon();
 
+    // Delete Wine by BrandName
     $result = $conn->query("SELECT brandName from WineA
                             UNION
                             SELECT brandName from WineB
@@ -24,6 +25,38 @@
         echo '<option value="'.$brandName.'">'.$brandName.'</option>';
     }
     echo "</select></p>";
+
+    echo "or";
+
+    // Delete Wine by WineID
+    $result = $conn->query("select wineID from WineB");
+    echo "<p>
+        <label>WineID</label>
+        <select name='wineID'>";
+    echo '<option value="">---Select wineID---</option>';
+    while ($row = $result->fetch_assoc())
+    {
+        unset($wineID);
+        $wineID = $row['wineID'];
+        echo '<option value="'.$wineID.'">'.$wineID.'</option>';
+    }
+    echo "</select></p>";
+
+    echo "or";
+
+    // Delete Wine by WineOrigin
+    $result = $conn->query("SELECT DISTINCT regionName from WineOrigin");
+    echo "<p><label>WineOrigin</label>";
+    echo "<select name='regionName'>";
+    echo '<option value="">---Select WineOrigin---</option>';
+    while ($row = $result->fetch_assoc())
+    {
+        unset($regionName);
+        $regionName = $row['regionName'];
+        echo '<option value="'.$regionName.'">'.$regionName.'</option>';
+    }
+    echo "</select></p>";
+
     CloseCon($conn);
     ?>
 
--- php/Wine/process-deleteWine.php
+++ php/Wine/process-deleteWine.php
@@ -3,13 +3,38 @@
 include_once '../../connect.php';
 
 $brandName = $_POST['brandName'];
+$wineID = $_POST['wineID'];
+$regionName = $_POST['regionName'];
 
 $conn = OpenCon();
 
-$sql = "DELETE from WineOrigin where wineID = 
-            (SELECT wineID from WineB where brandName = '$brandName');
-        DELETE from WineA where brandName = '$brandName'; 
-        DELETE from WineB where brandName = '$brandName'";
+$sql;
+if (!empty($brandName)) {
+    $sql = "DELETE from WineOrigin where wineID = 
+                (SELECT wineID from WineB where brandName = '$brandName');
+            DELETE from WineA where brandName = '$brandName'; 
+            DELETE from WineB where brandName = '$brandName'";
+} elseif (!empty($wineID)) {
+    $sql = "DELETE from WineOrigin where wineID = '$wineID';
+            DELETE from WineB where wineID = '$wineID'";
+} elseif (!empty($regionName)) {
+    
+    // Attempts: TODO
+    // $sql = "UPDATE WineB
+    //         SET wineID = -1 
+    //         WHERE wineID IN (SELECT wineID from WineOrigin 
+    //                         where regionName = '$regionName');
+    //         DELETE from WineOrigin where regionName = '$regionName';
+    //         DELETE from WineB where wineID = -1;";
+
+    // $sql = "CREATE VIEW ids AS (SELECT wineID from WineOrigin where regionName = '$regionName');
+    //         DELETE from WineOrigin where regionName = '$regionName';
+    //         DELETE from WineB where wineID IN (SELECT * FROM ids);";
+
+    // $sql = "DELETE from WineOrigin where regionName = '$regionName';
+    //         DELETE from WineB where wineID IN
+    //             (SELECT wineID from WineOrigin where regionName = '$regionName');";
+}
 
 if ($conn->multi_query($sql) === TRUE) {
 
