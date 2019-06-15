3165f616405b2f114d4ca1a03d324cddf139ad28
shirleywbi <swwbi@msn.com>
2019-06-14 17:04:20 -0700

    change supplier update query so both A+B are updated when address is inputed

--- php/Supplier/process-updateSupplier.php
+++ php/Supplier/process-updateSupplier.php
@@ -6,20 +6,33 @@ $supplierID = $_POST['supplierID'];
 $name = $_POST['name'];
 $address = $_POST['address'];
 $phoneNo = $_POST['phoneNo'];
-
+echo '<script>alert('.$supplierID.$name.$address.$phoneNo.');</script>';
 $conn = OpenCon();
-
+// If both supplierID and name are empty, alert error
+if (strpos($supplierID, '---') === false && strpos($name, '---') === false) {
+    echo '<script>alert("Please select only one.");</script>'; //TODO: This echo is not working
+}
+// If address is present, update address
 if (!empty($address)) {
-    $sql1 = "update SupplierA set address = '$address' where name = '$name'";
+    // if updating with name + address
+    // update address of SupplierB
+    $sql1 = "UPDATE supplierb SET address = '$address'
+            WHERE address = (SELECT a.address
+                            FROM SupplierA a
+                            WHERE a.name='$name');";
     $result1 = $conn->query($sql1);
-}
-if (!empty($phoneNo)) {
-    $sql2 = "update SupplierB set phoneNo = '$phoneNo' where supplierID = '$supplierID'";
+    // update address of SupplierA
+    $sql2 = "update SupplierA set address = '$address' where name = '$name'";
     $result2 = $conn->query($sql2);
+
+}
+// If phoneNo is present, update phoneNo
+if (!empty($phoneNo)) {
+    $sql3 = "update SupplierB set phoneNo = '$phoneNo' where supplierID = '$supplierID'";
+    $result3 = $conn->query($sql3);
 }
 
-
-if ($result1 || $result2) {
+if (($result1 && $result2) || $result3) {
     echo "Record updated successfully";
 } else {
     echo "Error updating record: " . $conn->error;
