740fc5cbf0c2d62f0a9400bac5f4476aabe15542
Annie Chang <anniechangannie@gmail.com>
2019-06-15 16:11:16 -0700

    delete

--- php/ReturnedShipment/delete.php
+++ php/ReturnedShipment/delete.php
@@ -1,18 +1,47 @@
 <?php
 include_once '../../template/input-query/create-table.php';
 
+$shipmentID = $returnID ='';
 
+
+// if (isset($_POST['return_delete'])) {
+//     $shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
+//     $returnID = mysqli_real_escape_string($conn, $_POST['returnID']);
+//     $sql = "DELETE FROM ReturnedShipment WHERE shipmentID = '$shipmentID' AND returnID='returnID';";
+//     $result = mysqli_query($conn, $sql);
+
+//     echo "<meta http-equiv='refresh' content='0'>";
+
+//     $_POST = array();
+// }
 
 if (isset($_POST['return_delete'])) {
-    $conn = OpenCon();
-    $shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
-    $returnID = mysqli_real_escape_string($conn, $_POST['returnID']);
-    $sql = "DELETE FROM ReturnedShipment WHERE shipmentID = '$shipmentID' AND returnID='returnID';";
-    $result = mysqli_query($conn, $sql);
+    if (empty($_POST['shipmentID']) or
+        empty($_POST['returnID'])) {
+        $errors = 'All fields are required';
+    }
+
+    if ($errors) {
+        //error is echoed
+    } else {
+        $shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
+        $returnID = mysqli_real_escape_string($conn, $_POST['returnID']);
+        // create sql
+        $sql = "DELETE FROM ReturnedShipment WHERE shipmentID = '$shipmentID' AND returnID='$returnID'";
+        // save to db and check
+        if (mysqli_query($conn, $sql)) {
+            // success
+            $success = 'Deleted!!';
+            echo "<meta http-equiv='refresh' content='0'>";
+
+        } else {
+            echo 'query error: ' . mysqli_error($conn);
+        }
 
 
+        $_POST = array();
+    }
 }
-
 
 ?>
 
@@ -30,6 +59,7 @@ if (isset($_POST['return_delete'])) {
                 <label for="returnID">returnID</label>
             </div>
         </div>
+        <div class="red-text"><?php echo $errors; ?></div>
         <input class="ui red button" type="submit" value="Delete" name="return_delete">
     </form>
 </div>
