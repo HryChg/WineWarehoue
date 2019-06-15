49d5c04f211965957177919e44bb91581fcb1513
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-15 03:19:21 -0700

    added LoginSQL file

--- SQL-Strings/IMQueries.sql
+++ SQL-Strings/IMQueries.sql
@@ -1,5 +1,5 @@
 -- Select queries:
-  -- queryWineBySugarRange
+  -- process-queryWineBySugarRange
     $sql1 = "SELECT wineID, brandName, grapeType1, grapeType2, sugarLevel
 	          FROM wineB
 	          WHERE sugarLevel >= '$lowRange' AND sugarLevel <= '$highRange'";
@@ -7,7 +7,7 @@
     $sql2 = "SELECT COUNT(wineID) AS 'Number of Wine Types'
 	          FROM wineB
 	          WHERE sugarLevel >= '$lowRange' AND sugarLevel <= '$highRange'";
-  -- queryWineByAlcoholRange
+  -- process-queryWineByAlcoholRange
     $sql1 = "SELECT wineID, brandName, grapeType1, grapeType2, alcoholPercent
 	          FROM wineB
 	          WHERE alcoholPercent >= '$lowRange' AND alcoholPercent <= '$highRange'";
@@ -15,14 +15,14 @@
     $sql2 = "SELECT COUNT(wineID) AS 'Number of Wine Types'
 	          FROM wineB
 	          WHERE alcoholPercent >= '$lowRange' AND alcoholPercent <= '$highRange'";
-  -- queryWineByAgriAttribute
+  -- process-queryWineByAgriAttribute
   SELECT w.wineID, a.name, a.$attribute, b.brandName, b.grapeType1, b.grapeType2
         FROM WineOrigin AS w
         INNER JOIN AgriculturalRegion AS a ON w.regionName=a.name
         INNER JOIN WineB AS b ON w.wineID=b.wineID
         WHERE a.$attribute = $val"
 
-  -- queryWineBByExpiryRange
+  -- process-queryWineBByExpiryRange
     $sql1 = "SELECT w.wineID, w.expiryDate, s.locationID, s.quantityInLocation
         FROM WineB as w
         INNER JOIN StoredIn AS s
@@ -34,7 +34,7 @@
         ON w.wineID = s.wineID
         WHERE expiryDate >= '$lowRangeDate' AND expiryDate <= '$highRangeDate'";
 
-  -- queryTopSupplier
+  -- process-queryTopSupplier
     SELECT sb.supplierID, sa.name, sa.address, sb.phoneNo, tot.total
         FROM SupplierB AS sb
         INNER JOIN SupplierA as sa ON sb.address = sa.address
@@ -45,12 +45,12 @@
                     LIMIT 1) AS tot
                     ON sb.supplierID = tot.supplierID
 
-  -- queryPriceFromWineBByID
+  -- process-queryPriceFromWineBByID
     SELECT wineID, price
 	    FROM wineB
 	    WHERE wineID = '$wineID'
 
-  -- queryMinPriceByBrand
+  -- process-queryMinPriceByBrand
     SELECT w.brandName, w.grapeType1, w.grapeType2, w.price
         from
         (select brandName, grapeType1, grapeType2, price from wineb where brandName = '$brandName') w
@@ -58,14 +58,14 @@
         (SELECT brandName, MIN(price) AS min from wineb GROUP BY brandName) m
         ON w.brandName = m.brandName AND w.price = m.min
 
-  -- queryMaxPriceOfWineB
+  -- process-queryMaxPriceOfWineB
     SELECT wineID, brandName, grapeType1, grapeType2, price
             FROM WineB AS p
             INNER JOIN
             ( SELECT MAX(price) AS max FROM WineB ) AS m
             ON p.price = m.max
 
-  -- queryLocationAndQuantityByWineID
+  -- process-queryLocationAndQuantityByWineID
     if (!empty($wineID) && !empty($locationID)) {
       $sql = "SELECT wineID, locationID, quantityInLocation
 	        FROM StoredIn
@@ -83,7 +83,7 @@
      $result = $conn->query($sql);
     }
 
-  -- queryExpiredWineInStorage
+  -- process-queryExpiredWineInStorage
     if ($expiry == '') {
     $sql = "SELECT w.wineID, w.expiryDate, s.locationID, s.quantityInLocation
         FROM WineB as w
@@ -97,18 +97,21 @@
         ON w.wineID = s.wineID
         WHERE expiryDate < '$expiry'";
 
-  -- queryBrandFromWineAByWineTaste
+  -- process-queryBrandFromWineAByWineTaste
   SELECT brandName, wineTaste1, wineTaste2
 	FROM wineA
 	WHERE wineTaste1 = '$wineTaste' OR wineTaste2 = '$wineTaste'
 
-  -- queryBrandFromWineAByGrape
+  -- process-queryBrandFromWineAByGrape
   SELECT brandName, grapeType1, grapeType2
 	FROM wineA
 	WHERE grapeType1 = '$grapeType' OR grapeType2 = '$grapeType'
 
 
 -- insert:
+  -- AgriculturalRegion:
+
+  -- Restock
 
 -- delete:
 
