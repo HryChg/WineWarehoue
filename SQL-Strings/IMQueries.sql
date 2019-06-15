62a1223f5358163fab3d5d8d3a0ce5b0e2862e70
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-15 03:40:14 -0700

    added LoginSQL file

--- SQL-Strings/IMQueries.sql
+++ SQL-Strings/IMQueries.sql
@@ -109,12 +109,109 @@
 
 
 -- insert:
-  -- AgriculturalRegion:
+  -- Restock:
+    INSERT INTO Restock VALUES ('$employee_id', '$supplier_id', '$wine_id', '$location', '$quantity', DATE('$date'))
 
-  -- Restock
+  -- StoredIn:
+    INSERT INTO StoredIn VALUES ('$id', '$location', '$quantity')
+
+  -- Supplier:
+      $sql = "INSERT INTO SupplierA VALUES ('$address', '$name')";
+      $sql = "INSERT INTO SupplierB VALUES ('$id', '$phone', '$address')";
+
+  -- Wine:
+      $sql1 = "INSERT INTO WineA VALUES ('$grape1', '$grape2', '$brand', '$taste1', '$taste2')";
+      $sql2 = "INSERT INTO WineB VALUES ('$wineid', '$price', '$color', '$brand', '$grape1', '$grape2', '$alcohol', '$acid', '$sugar', DATE('$expiry'))";
+      $sql3 = "INSERT INTO AgriculturalRegion VALUES ('$region', '$temperature', '$moisture', '$climate')";
+      $sql4 = "INSERT INTO WineOrigin VALUES ('$region', '$wineid', '$year')";
+
+
 
 -- delete:
+  -- AgriculturalRegion:
+    delete from AgriculturalRegion where name = '$name'
+
+  -- Supplier:
+      if (empty($name) && empty($supplierID) && empty($phoneNo)) {
+       // do nothing
+      } elseif (!empty($name)) {
+        $sql = "DELETE from SupplierB where address =
+        (SELECT address FROM SupplierA where name = '$name');
+        DELETE from SupplierA where name = '$name';";
+      } else {
+        $sql = "DELETE from SupplierA where address =
+          (SELECT address FROM SupplierB where supplierID = '$supplierID' or phoneNo = '$phoneNo');
+          DELETE from SupplierB where supplierID = '$supplierID' or phoneNo = '$phoneNo'";
+      }
+
+  -- Wine:
+     -- process-deleteWineByBrand
+        $sql = "delete from WineA where brandName = '$brandName'; delete from WineB where brandName = '$brandName'";
+
+     -- process-deleteWineByID
+        $sql = "delete from WineB where wineID = '$wineID'";
+
+
+
+
+
 
 -- update:
+  -- AgriculturalRegion:
+      if (!empty($temperature)) {
+      $sql1 = "update AgriculturalRegion set temperature = '$temperature' where name = '$name'";
+      $result1 = $conn->query($sql1);
+      }
+      if (!empty($moisture)) {
+      $sql2 = "update AgriculturalRegion set moisture = '$moisture' where name = '$name'";
+      $result2 = $conn->query($sql2);
+      }
+      if (!empty($climate)) {
+       $sql3 = "update AgriculturalRegion set climate = '$climate' where name = '$name'";
+      $result3 = $conn->query($sql3);
+      }
+
+  -- StorageArea:
+    update StorageArea set temperature = '$temperature' where locationID = '$locationID'
+
+  -- StoredIn:
+    update StoredIn set quantityInLocation = '$quantityInLocation' where locationID = '$locationID' and wineID = '$wineID'
+
+  -- Supplier:
+      $sqlB = "UPDATE supplierb SET address = '$address'
+        WHERE address = (SELECT a.address
+        FROM SupplierA a
+        WHERE a.name='$name');
+
+      $sqlA = "update SupplierA set address = '$address' where name = '$name'
+
+      $sqlA = "UPDATE suppliera SET address = '$address'
+                WHERE address = (SELECT b.address
+                                FROM SupplierB b
+                                WHERE b.supplierID='$supplierID')
+
+      $sqlB = "update SupplierB set address = '$address' where supplierID = '$supplierID'
+
+      $sqlB = "UPDATE supplierb b SET phoneNo = '$phoneNo'
+                WHERE b.address = (SELECT a.address
+                                    FROM SupplierA a
+                                    WHERE a.name='$name')
+
+      $sqlB = "update SupplierB set phoneNo = '$phoneNo' where supplierID = '$supplierID'
+
+  -- Wine:
+      $sql = "update WineB set price = '$price' where wineID = '$wineID'";
 
 
+  -- WineOrigin:
+      $sql = "delete from WineOrigin where regionName = '$regionName' and wineID = '$wineID'";
+
+
+-- view:
+  -- AgriculturalRegion
+  -- Restock
+  -- StorageArea
+  -- StoredIn
+  -- Supplier
+  -- Wine
+  -- WineOrigin
