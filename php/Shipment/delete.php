c6a6f6f933e275cd199d3b9e7337011cc093ff2b
harrychuang <vrjgik5@gmail.com>
2019-06-14 18:05:22 -0700

    in page refresh available in delete Shipment

--- php/Shipment/delete.php
+++ php/Shipment/delete.php
@@ -1,10 +1,36 @@
-<!DOCTYPE html>
-<html>
+<?php
+include_once '../../connect.php';
+include_once '../../template/input-query/create-table.php';
+
+
+
+if (isset($_POST['shipmentID'])) {
+    $conn = OpenCon();
+    $shipmentID = mysqli_real_escape_string($conn, $_POST['shipmentID']);
+    $sql = "DELETE FROM Shipment WHERE shipmentID = '$shipmentID';";
+    $result = mysqli_query($conn, $sql);
+
+
+    if (!$result) {
+        echo "Record unable to be deleted.";
+        echo "<br/>";
+    }
+    if ($result) {
+        echo "Record has been deleted";
+        echo "<br/>";
+    }
+
+
+    CloseCon($conn);
+}
+
+
+?>
 
 
 <h1 class="ui header">Delete Shipment</h1>
 <div class="ui segment">
-    <form class="ui form container" action="../../php/Shipment/process-delete.php" method="POST">
+    <form class="ui form container" action="../../ui/ShippingManager/index.php" method="POST">
         <div class="fields">
             <div class="field">
                 <input name="shipmentID" type="text">
