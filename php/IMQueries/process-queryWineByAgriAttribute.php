ab78064d07193de86851364652b5bc57c1aba336
MayKikuchi <m.may.kikuchi@gmail.com>
2019-06-15 22:00:11 -0700

    fixed query wine by agriculutral region attribute query

--- php/IMQueries/process-queryWineByAgriAttribute.php
+++ php/IMQueries/process-queryWineByAgriAttribute.php
@@ -10,11 +10,19 @@ $conn = OpenCon();
 $attribute = $_POST['attribute'];
 $val = $_POST['val'];
 
+if ($attribute == 'climate') {
+    $sql = "SELECT w.wineID, a.name, a.$attribute, b.brandName, b.grapeType1, b.grapeType2
+        FROM WineOrigin AS w 
+        INNER JOIN AgriculturalRegion AS a ON w.regionName=a.name
+        INNER JOIN WineB AS b ON w.wineID=b.wineID
+        WHERE a.$attribute = '$val'";
+} else {
 $sql = "SELECT w.wineID, a.name, a.$attribute, b.brandName, b.grapeType1, b.grapeType2
         FROM WineOrigin AS w 
         INNER JOIN AgriculturalRegion AS a ON w.regionName=a.name
         INNER JOIN WineB AS b ON w.wineID=b.wineID
         WHERE a.$attribute = $val";
+}
 
 $result = $conn->query($sql);
 
